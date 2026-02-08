76ccb1eddb36c2eaaf02b2f516b2563726019f7c
Gregory Pakosz <gregory.pakosz@gmail.com>
2026-02-08 23:12:32 +0100

    Update params.seed when -S is not given

--- main.c
+++ main.c
@@ -450,6 +450,7 @@ int main(int argc, char *argv[]) {
         actual_seed = params.seed;
     } else {
         actual_seed = (int64_t)time(NULL);
+        params.seed = actual_seed;
     }
     flux_set_seed(actual_seed);
     LOG_NORMAL("Seed: %lld\n", (long long)actual_seed);
